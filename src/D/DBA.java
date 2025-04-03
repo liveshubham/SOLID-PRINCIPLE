d9aed680bc9a630bf3b490a560d89524baa81549
ShubhamTiwari <77232421+liveshubham@users.noreply.github.com>
2025-04-03 11:47:19 +0530

    Update DBA.java

--- src/D/DBA.java
+++ src/D/DBA.java
@@ -15,8 +15,10 @@ public class DBA {
 	public static void main(String[] args) {
 		DataBase s = new MySqldatabase();
 		DBA d = new DBA(s);
+		d.print();//ye my sql database ko call krega.
 		DataBase s1 = new No_SQL();
 		DBA d1 = new DBA(s1);
+		d1.print(); // ye no sql database ko call krega.
  
 	}
 }
